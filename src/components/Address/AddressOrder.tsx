3130c99633b237488f19352c557a83184d33a4ad
Parichat Saubol <parichat01032003@example.com>
2025-02-03 14:23:01 +0700

    Update translation files

--- src/components/Address/AddressOrder.tsx
+++ src/components/Address/AddressOrder.tsx
@@ -1,14 +1,18 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useTheme } from '@/hooks';
 import { useTranslation } from 'react-i18next';
-import { t } from 'i18next';
 
 interface Props {
+  // eslint-disable-next-line react/require-default-props
   name?: string;
+  // eslint-disable-next-line react/require-default-props
   phoneNumber?: string;
+  // eslint-disable-next-line react/require-default-props
   address?: string;
+  // eslint-disable-next-line react/require-default-props
   isEdit?: boolean;
+  // eslint-disable-next-line react/require-default-props
   onPress?: () => void;
 }
 
@@ -23,53 +27,58 @@ const AddressOrder: FunctionComponent<Props> = ({
   const { Layout, Fonts, Images } = useTheme();
   const { t } = useTranslation('address');
 
-  // const [address, setAddress] = useState<number>();
-  return (
-    <>
-      <View style={[Layout.col, styles.container]}>
-        <View
-          style={[
-            Layout.row,
-            Layout.justifyContentBetween,
-            Layout.alignItemsCenter,
-          ]}
-        >
-          <View style={[Layout.row, Layout.gap10, Layout.alignItemsCenter]}>
-            <Images.icons.location color="#0057FF" />
-            <Text style={[Fonts.text21, Fonts.textPrimary]}>
-              {t('addressOrder.deliveryAddress')}
-            </Text>
-          </View>
-          {isEdit && (
-            <TouchableOpacity
-              onPress={() => {
-                onPress && onPress();
-              }}
-            >
-              <Text style={[Fonts.text16, Fonts.textPrimary]}>
-                {t('addressOrder.changeAddress')}
-              </Text>
-            </TouchableOpacity>
-          )}
-        </View>
-        <View style={[Layout.row, Layout.justifyContentBetween]}>
-          <Text style={[Fonts.text18Bold]}>{name}</Text>
-          <Text style={Fonts.text18}>{phoneNumber}</Text>
-        </View>
-        <View>
-          <Text style={Fonts.text18}>{address}</Text>
-        </View>
-      </View>
-    </>
+  // ใช้ useState เพื่อเก็บค่าดีฟอลต์
+  const [defaultName, setDefaultName] = useState<string | undefined>(name);
+  const [defaultAddress, setDefaultAddress] = useState<string | undefined>(
+    address,
   );
-};
 
-AddressOrder.defaultProps = {
-  name: t('addressOrder.defaultName', { returnObjects: true }),
-  phoneNumber: '081-567-8912',
-  address: t('addressOrder.defaultAddress', { returnObjects: true }),
-  isEdit: true,
-  onPress: undefined,
+  useEffect(() => {
+    // ตั้งค่าดีฟอลต์จาก t() ถ้าไม่ได้รับ props name หรือ address
+    if (!name) {
+      setDefaultName(t('addressOrder.defaultName'));
+    }
+    if (!address) {
+      setDefaultAddress(t('addressOrder.defaultAddress'));
+    }
+  }, [t, name, address]); // ใช้ t, name, address เป็น dependency
+
+  return (
+    <View style={[Layout.col, styles.container]}>
+      <View
+        style={[
+          Layout.row,
+          Layout.justifyContentBetween,
+          Layout.alignItemsCenter,
+        ]}
+      >
+        <View style={[Layout.row, Layout.gap10, Layout.alignItemsCenter]}>
+          <Images.icons.location color="#0057FF" />
+          <Text style={[Fonts.text21, Fonts.textPrimary]}>
+            {t('addressOrder.deliveryAddress')}
+          </Text>
+        </View>
+        {isEdit && (
+          <TouchableOpacity
+            onPress={() => {
+              onPress && onPress();
+            }}
+          >
+            <Text style={[Fonts.text16, Fonts.textPrimary]}>
+              {t('addressOrder.changeAddress')}
+            </Text>
+          </TouchableOpacity>
+        )}
+      </View>
+      <View style={[Layout.row, Layout.justifyContentBetween]}>
+        <Text style={[Fonts.text18Bold]}>{defaultName}</Text>
+        <Text style={Fonts.text18}>{phoneNumber}</Text>
+      </View>
+      <View>
+        <Text style={Fonts.text18}>{defaultAddress}</Text>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
